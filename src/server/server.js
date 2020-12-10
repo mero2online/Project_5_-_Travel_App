5c4a00ece3f81b977cf81cbcff39ae2495865215
Mohamed-Omar <mero2online@gmail.com>
2020-12-10 20:25:38 +0200

    Setup server

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,12 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
+var path = require('path');
+
 // Require Express to run server and routes
 const express = require('express');
+
+const mockAPIResponse = require('./mockAPI.js');
 
 // Start up an instance of app
 const app = express();
@@ -20,16 +24,26 @@ const cors = require('cors');
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static("website"));
+app.use(express.static('dist'));
+
+console.log(__dirname);
+
+app.get('/', function (req, res) {
+  res.sendFile('dist/index.html');
+});
 
 // Setup Server
-const port = 8000;
+const port = 8081;
 const server = app.listen(port, listening);
 
 function listening() {
     console.log(`server running on localhost: ${port}`);
     console.log(`http://localhost:${port}/`);
 }
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse);
+  });
 
 // GET Route
 app.get('/all', getData)
